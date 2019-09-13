613fa853eab773c48baf847e9a4eebd9f452e35d
daviitp <david_xen06@hotmail.com>
2019-09-13 16:40:50 -0300

    Update dnsQuery.py

--- dnsQuery.py
+++ dnsQuery.py
@@ -1,24 +1,20 @@
 import dns.resolver
-import sys
 
-def resolveDNS(dominio):
-    
-    resolver = dns.resolver.Resolver(); 
-    answer = resolver.query(dominio , "A")
-    return answer
+def get_domain_dns(domain):
+        
+        dns_records = list()
 
-urls = open("urls.txt","r")
-content = urls.read()
-dominios = content.splitlines()
+        try:
+            # get the dns resolutions for this domain
+            dns_results = dns.resolver.query(domain)
+            dns_records = [ip.address for ip in dns_results]
+        except dns.resolver.NXDOMAIN as e:
+            # the domain does not exist so dns resolutions remain empty
+            pass
+        except dns.resolver.NoAnswer as e:
+            # the resolver is not answering so dns resolutions remain empty
+            pass
 
-archivo=open("resultados.txt","w")
-archivo.close()
-
-for dominio in dominios:
-    resultDNS = resolveDNS(dominio)
-    answer = ''
-    for item in resultDNS:
-        IP = ','.join([str(item), answer])
-        #sys.stdout=open("resultados.txt","a")
-        print(dominio + ": " + IP)
-        #sys.stdout.close()
+        return dns_records 
+        
+print(get_domain_dns('www.bacan.com.ar'))
