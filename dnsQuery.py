b69b7e94e2daf9791f5f24f73b4646c5d7aa0768
daviitp <david_xen06@hotmail.com>
2019-09-18 16:42:14 -0300

    Update dnsQuery.py

--- dnsQuery.py
+++ dnsQuery.py
@@ -1,7 +1,16 @@
 import dns.resolver
+#from time import perf_counter 
+#import sys
+
+#t1_start = perf_counter()  
 
 def get_domain_dns(domain):
         
+        dns.resolver.default_resolver = dns.resolver.Resolver(configure=False)
+        dns.resolver.default_resolver.nameservers = ['200.1.116.10']
+        
+        g=open("resultados.txt","a+")
+                
         dns_records = list()
 
         try:
@@ -10,11 +19,33 @@ def get_domain_dns(domain):
             dns_records = [ip.address for ip in dns_results]
         except dns.resolver.NXDOMAIN as e:
             # the domain does not exist so dns resolutions remain empty
+            g.write(url + "\n")
             pass
         except dns.resolver.NoAnswer as e:
             # the resolver is not answering so dns resolutions remain empty
+            g.write(url + "\n")
+            pass
+        except dns.resolver.NoNameservers as e:
+            #sys.stdout=open("resultados.txt","a")
+            #print(url)
+            #sys.stdout.close()
+            g.write(url + "\n")
             pass
 
         return dns_records 
         
-print(get_domain_dns('www.bacan.com.ar'))
+### Abre el archivo que contiene las URLs ###
+
+f = open("urls.txt","r")
+dominios = f.read()
+urls = dominios.splitlines()
+
+### Abre el archivo donde se almacenarán los resultados encontrados ###
+g=open("resultados.txt","w")
+
+### Realiza las consultas DNS para cada URL del archivo
+for url in urls:
+    get_domain_dns(url)
+
+#t1_stop = perf_counter() 
+#print("Tiempo total:", t1_stop-t1_start)
