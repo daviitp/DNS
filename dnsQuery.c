aa611c2f3645e5148de69ae2c09022dc492eaa90
daviitp <david_xen06@hotmail.com>
2019-09-18 16:10:29 -0300

    Update dnsQuery.c

--- dnsQuery.c
+++ dnsQuery.c
@@ -1,85 +1,206 @@
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
-#include <netdb.h>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <arpa/inet.h>
+#include<stdio.h> 		//printf
+#include<string.h>    	//strlen
+#include<stdlib.h>    	//malloc
+#include<sys/socket.h>	//you know what this is for
+#include<arpa/inet.h> 	//inet_addr , inet_ntoa , ntohs etc
+#include<netinet/in.h>
+#include<unistd.h>    	//getpid
+ 
+//List of DNS Servers 
+char dns_servers[10][100];
+int dns_server_count = 0;
 
-int
-lookup_host (const char *host)
+//Types of DNS resource records
+#define T_A 1 		//Ipv4 address
+//#define T_NS 2 	//Nameserver
+//#define T_CNAME 5 // canonical name
+//#define T_SOA 6 	/* start of authority zone */
+//#define T_PTR 12 	/* domain name pointer */
+//#define T_MX 15 	//Mail server
+ 
+//Function Prototypes
+void ngethostbyname (unsigned char* , int);
+void ChangetoDnsNameFormat (unsigned char*,unsigned char*);
+void get_dns_servers();
+ 
+//DNS header structure
+struct DNS_HEADER
 {
-    struct addrinfo hints, *res;
-    int errcode;
-    char addrstr[150];
-    void *ptr;
-
-    FILE *results;
-        results = fopen("resultados.txt","w");
-
-    FILE *detection;
-        detection = fopen("detecciones.txt","w");
-
-    memset (&hints, 0, sizeof (hints));
-    hints.ai_family = PF_UNSPEC;
-    hints.ai_socktype = SOCK_STREAM;
-    hints.ai_flags |= AI_CANONNAME;
-
-    errcode = getaddrinfo (host, NULL, &hints, &res);
-    if (errcode != 0)
-        {
-        fprintf (detection, "Host: %s\n", host);
-        return -1;
-        }
-
-    while (res)
-        {
-        inet_ntop (res->ai_family, res->ai_addr->sa_data, addrstr, 150);
-
-        switch (res->ai_family)
-            {
-            case AF_INET:
-            ptr = &((struct sockaddr_in *) res->ai_addr)->sin_addr;
-            break;
-            case AF_INET6:
-            ptr = &((struct sockaddr_in6 *) res->ai_addr)->sin6_addr;
-            break;
-            }
-        inet_ntop (res->ai_family, ptr, addrstr, 150);
-        fprintf (results, "Host: %s\n", host);
-        fprintf (results, "IPv%d address: %s (%s)\n", res->ai_family == PF_INET6 ? 6 : 4,
-                addrstr, res->ai_canonname);
-        res = res->ai_next;
-        }
-
-    return 0;
-}
-
-int main (void)
+    unsigned short id; // identification number
+ 
+    unsigned char rd :1; // recursion desired
+    unsigned char tc :1; // truncated message
+    unsigned char aa :1; // authoritive answer
+    unsigned char opcode :4; // purpose of message
+    unsigned char qr :1; // query/response flag
+ 
+    unsigned char rcode :4; // response code
+    unsigned char cd :1; // checking disabled
+    unsigned char ad :1; // authenticated data
+    unsigned char z :1; // its z! reserved
+    unsigned char ra :1; // recursion available
+ 
+    unsigned short q_count; // number of question entries
+    unsigned short ans_count; // number of answer entries
+    unsigned short auth_count; // number of authority entries
+    unsigned short add_count; // number of resource entries
+};
+ 
+//Constant sized fields of query structure
+struct QUESTION
 {
-
+    unsigned short qtype;
+    unsigned short qclass;
+};
+ 
+//Constant sized fields of the resource record structure
+#pragma pack(push, 1)
+struct R_DATA
+{
+    unsigned short type;
+    unsigned short _class;
+    unsigned int ttl;
+    unsigned short data_len;
+};
+#pragma pack(pop)
+ 
+//Pointers to resource record contents
+struct RES_RECORD
+{
+    unsigned char *name;
+    struct R_DATA *resource;
+    unsigned char *rdata;
+};
+ 
+//Structure of a Query
+typedef struct
+{
+    unsigned char *name;
+    struct QUESTION *ques;
+} QUERY;
+ 
+int main( int argc , char *argv[])
+{
+    unsigned char hostname[100];
+ 
+    //Get the DNS servers
+    get_dns_servers();
+     
+    //Get the hostname from file and query DNS for each one. 
     char inbuf[256];
     int len;
 
-        FILE *archivo;
-        archivo = fopen("urls.txt","r");
+    FILE *archivo;
+    archivo = fopen("urls.txt","r");
 
-        if (archivo == NULL)
-                exit(1);
-        else
-        {
-            while (feof(archivo) == 0)
-            {
-            do {
-            bzero(inbuf, 256);
-            fgets(inbuf, 256, archivo);
-            len = strlen(inbuf);
-            inbuf[len-1] = '\0';
-            if(strlen(inbuf) > 0)
-                lookup_host (inbuf);
-            else
-                return EXIT_SUCCESS;
-			} while(1);
-		}
+    while (feof(archivo) == 0)
+    {
+        do {
+    bzero(inbuf, 256);
+    fgets(inbuf, 256, archivo);
+    len = strlen(inbuf);
+    inbuf[len-1] = '\0';
+    if(strlen(inbuf) > 0)
+            ngethostbyname(inbuf, T_A);
+    else
+        return EXIT_SUCCESS;
+		} while(1);
+	}
+	
+    return 0;
+}
+ 
+void ngethostbyname(unsigned char *host , int query_type)
+{
+    unsigned char buf[65536],*qname,*reader;
+    int i , j , stop , s;
+ 
+    struct sockaddr_in a;
+ 
+    struct RES_RECORD answers[20],auth[20],addit[20]; 
+    struct sockaddr_in dest;
+ 
+    struct DNS_HEADER *dns = NULL;
+    struct QUESTION *qinfo = NULL;
+ 
+    s = socket(AF_INET , SOCK_DGRAM , IPPROTO_UDP); //UDP + IPv4
+ 
+    dest.sin_family = AF_INET;
+    dest.sin_port = htons(53);
+    dest.sin_addr.s_addr = inet_addr(dns_servers[0]); //dns servers
+ 
+    //Set the DNS structure to standard queries
+    dns = (struct DNS_HEADER *)&buf;
+ 
+    dns->id = (unsigned short) htons(getpid());
+    dns->qr = 0; //Query
+    dns->opcode = 0; //Standard query
+    dns->aa = 0; //Not Authoritative
+    dns->tc = 0; //This message is not truncated
+    dns->rd = 1; //Recursion Desired
+    dns->ra = 0;
+    dns->z = 0;
+    dns->ad = 0;
+    dns->cd = 0;
+    dns->rcode = 0;
+    dns->q_count = htons(1);
+    dns->ans_count = 0; //Cantidad de respuestas de la query.
+    dns->auth_count = 0;
+    dns->add_count = 0;
+ 
+    //point to the query portion
+    qname =(unsigned char*)&buf[sizeof(struct DNS_HEADER)];
+ 
+    ChangetoDnsNameFormat(qname , host);
+    qinfo =(struct QUESTION*)&buf[sizeof(struct DNS_HEADER) + (strlen((const char*)qname) + 1)]; //fill it
+ 
+    qinfo->qtype = htons( query_type ); //type of the query , A , MX , CNAME , NS etc
+    qinfo->qclass = htons(1);
+     
+    if( sendto(s,(char*)buf,sizeof(struct DNS_HEADER) + (strlen((const char*)qname)+1) + sizeof(struct QUESTION),0,(struct sockaddr*)&dest,sizeof(dest)) < 0)
+    {
+        perror("sendto failed");
     }
+     
+    //Receive the answer
+    i = sizeof dest;
+    if(recvfrom (s,(char*)buf , 65536 , 0 , (struct sockaddr*)&dest , (socklen_t*)&i ) < 0)
+    {
+        perror("recvfrom failed");
+    }
+     
+    dns = (struct DNS_HEADER*) buf;
+ 
+    //move ahead of the dns header and the query field
+    reader = &buf[sizeof(struct DNS_HEADER) + (strlen((const char*)qname)+1) + sizeof(struct QUESTION)];
+
+    // Guarda en archivo los dominios que no se pudo resolver la IP. 
+    if (ntohs(dns->ans_count == 0)) {
+		printf("Error: %s IP not found\n",host);
+	}
+}
+ 
+void get_dns_servers()
+{
+    strcpy(dns_servers[0] , "200.1.116.10");	
+}
+ 
+void ChangetoDnsNameFormat(unsigned char* dns,unsigned char* host) 
+{
+    int lock = 0 , i;
+    strcat((char*)host,".");
+     
+    for(i = 0 ; i < strlen((char*)host) ; i++) 
+    {
+        if(host[i]=='.') 
+        {
+            *dns++ = i-lock;
+            for(;lock<i;lock++) 
+            {
+                *dns++=host[lock];
+            }
+            lock++; //or lock=i+1;
+        }
+    }
+    *dns++='\0';
 }
